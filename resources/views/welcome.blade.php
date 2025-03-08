36f09cefa88d52f697de4c40177372d97bc6dc30
CarolinaSanches24 <carolmosilva34@gmail.com>
2025-03-08 18:17:09 -0300

    fix: corrigindo listagem de produtos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,15 +5,15 @@
 <div id="events-container" class="col-md-12">
     <h1>Próximos eventos</h1>
     <div id="cards-container" class="row">
-        @foreach($events as $event)
+        @foreach($events as $product)
         <div class="col-md-3">
             <div class="card">
-                <img src="/img/1.jpg" class="card-img-top" alt="{{$event->title}}">
+                <img src="{{asset('storage/'.$product->image)}}" class="card-img-top" alt="{{$product->title}}">
                 <div class="card-body text-center">
                     <p class="card-date">14/01/2025</p>
-                    <h5 class="card-title">{{$event->title}}</h5>
-                    <p class="card-participantes">x participantes</p>
-                    <a href="#" class="btn btn-primary">Saber mais</a>
+                    <h5 class="card-title">{{$product->name}}</h5>
+                    <p class="card-participantes"> R${{$product->value}}</p>
+                    <a href="#" class="btn btn-primary">COMPRAR</a>
                 </div>
             </div>
         </div>
